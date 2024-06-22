a8c268282541f73068577a9bddcdbfb00905600b
DuckySmacky <kriperoka@gmail.com>
2024-06-22 18:54:50 +0300

    add sortByKey

--- src/main/java/io/github/duckysmacky/ssutils/MapUtil.java
+++ src/main/java/io/github/duckysmacky/ssutils/MapUtil.java
@@ -12,9 +12,9 @@ import java.util.*;
 public final class MapUtil {
 
     /**
-     * Sorts a map by its values based on given order
+     * Sorts a map by its values based on a given order
      * @param inputMap map to be sorted
-     * @param order order to sort the map by
+     * @param order order to sort the map by (ascending or descending)
      * @return sorted map
      * @param <K> key
      * @param <V> comparable value
@@ -25,6 +25,25 @@ public final class MapUtil {
         switch (order) {
             case ASCENDING -> entryList.sort(Map.Entry.comparingByValue());
             case DESCENDING -> entryList.sort(Collections.reverseOrder(Map.Entry.comparingByValue()));
+        }
+        for (Map.Entry<K, V> entry : entryList) sortedMap.put(entry.getKey(), entry.getValue());
+        return sortedMap;
+    }
+
+    /**
+     * Sorts a map by its keys based on a given order
+     * @param inputMap map to be sorted
+     * @param order order to sort the map by (ascending or descending)
+     * @return sorted map
+     * @param <K> comparable key
+     * @param <V> value
+     */
+    public static <K extends Comparable<? super K>, V> Map<K, V> sortByKey(Map<K, V> inputMap, Order order) {
+        List<Map.Entry<K, V>> entryList = new ArrayList<>(inputMap.entrySet());
+        Map<K, V> sortedMap = new HashMap<>();
+        switch (order) {
+            case ASCENDING -> entryList.sort(Map.Entry.comparingByKey());
+            case DESCENDING -> entryList.sort(Collections.reverseOrder(Map.Entry.comparingByKey()));
         }
         for (Map.Entry<K, V> entry : entryList) sortedMap.put(entry.getKey(), entry.getValue());
         return sortedMap;
